9eea3323d8dd60a516ead7fb888b3d4a60064e7a
Alexander <alrttv@gmail.com>
2018-10-07 18:48:06 +0300

    Загрузка комментов. Почти работает.

--- js/photo.js
+++ js/photo.js
@@ -14,6 +14,17 @@
 
   var bigPicture = document.querySelector('.big-picture');
   var commentsContainer = bigPicture.querySelector('.social__comments');
+  var commentsLoader = bigPicture.querySelector('.social__comments-loader');
+
+  var counter = {
+    nextComments: MAX_COMMENTS_IN_BLOCK,
+    addComments: function () {
+      this.nextComments += MAX_COMMENTS_IN_BLOCK;
+    },
+    resetComments: function () {
+      this.nextComments = MAX_COMMENTS_IN_BLOCK;
+    }
+  };
 
   window.photo = {
     renderBigPhoto: function (currentPhoto) {
@@ -21,7 +32,6 @@
       var calculateCommentsNumber = function () {
 
         return currentPhoto.comments.length;
-
       };
 
       var bigPictureImage = bigPicture.querySelector('.big-picture__img');
@@ -34,6 +44,30 @@
       displayedCommentsCounter.textContent = (currentPhoto.comments.length < MAX_COMMENTS_IN_BLOCK) ? currentPhoto.comments.length : MAX_COMMENTS_IN_BLOCK;
       window.util.createElements(commentsContainer, window.render.renderSocialComment, currentPhoto.comments.slice(0, 5));
 
+      var onLoaderClick = function () {
+
+        window.util.createElements(commentsContainer, window.render.renderSocialComment, currentPhoto.comments.slice(counter.nextComments, counter.nextComments + MAX_COMMENTS_IN_BLOCK));
+
+        displayedCommentsCounter.textContent = parseInt(displayedCommentsCounter.textContent, 10) + MAX_COMMENTS_IN_BLOCK;
+
+        if (displayedCommentsCounter.textContent > currentPhoto.comments.length) {
+          displayedCommentsCounter.textContent = currentPhoto.comments.length;
+        }
+
+        if (counter.nextComments + MAX_COMMENTS_IN_BLOCK >= currentPhoto.comments.length) {
+          commentsLoader.classList.add('hidden');
+        }
+
+        counter.addComments();
+
+      };
+
+      if (currentPhoto.comments.length <= 5) {
+        commentsLoader.classList.add('hidden');
+      } else {
+        commentsLoader.classList.remove('hidden');
+        commentsLoader.addEventListener('click', onLoaderClick);
+      }
 
     },
     showBigPicture: function () {
@@ -49,16 +83,14 @@
       document.removeEventListener('keydown', onBigPictureEscPress);
       commentsContainer.innerHTML = null;
       document.querySelector('body').classList.remove('modal-open');
-
+      counter.resetComments();
     }
   };
 
   var onBigPictureEscPress = function (evt) {
 
     if (evt.keyCode === window.util.ESC_KEYCODE) {
-
       window.photo.hideBigPicture();
-
     }
 
   };
